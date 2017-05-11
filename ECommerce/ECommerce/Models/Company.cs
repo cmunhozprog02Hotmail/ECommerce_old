bf64a2245162680eb1f7b7ca620b0076340f9711
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-05-10 23:39:40 -0300

    Aula 08 - 01

---
+++ ECommerce/ECommerce/Models/Company.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é origatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deverá ter no máximo {1} caracteres")]
+        [Display(Name = "Company")]
+        [Index("Company_Name_Index", IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é origatório")]
+        [MaxLength(20, ErrorMessage = "O campo {0} deverá ter no máximo {1} caracteres")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é origatório")]
+        [MaxLength(100, ErrorMessage = "O campo {0} deverá ter no máximo {1} caracteres")]
+        [DataType(DataType.PhoneNumber)]
+        public string Address { get; set; }
+
+
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
+}
